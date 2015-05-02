7b9d2766d154182eb886bfb795ff1c9451ea066f
Leandro Silva <lfsilva@me.com>
2015-05-01 21:21:17 -0300

    Estrutura e organização inicial do projeto.

--- viajabessa-ios/Controllers/ListViewController.h
+++ viajabessa-ios/Controllers/ListViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ListViewController.h
 //  viajabessa-ios
 //
 //  Created by Leandro Silva on 5/1/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ListViewController : UIViewController
 
 @end
-
